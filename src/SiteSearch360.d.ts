895589c7d1eae587162a20a4a49297e5dd7a8cdf
JVankat <jaroslav.vankat@semknox.com>
2022-06-24 14:13:38 +0200

    fix type declearations

--- src/SiteSearch360.d.ts
+++ src/SiteSearch360.d.ts
@@ -1,13 +1,18 @@
-declare module 'SiteSearch360' {
-	import React from 'react';
+import React from 'react';
 
-	interface SiteSearch360Props {
-		ss360Config: object;
-		siteId: string;
-		alias: string;
-		showButton: boolean;
-		applyStyling: boolean
-	}
-
-	export const SiteSearch360: (props: SiteSearch360Props) => React.SFC<SiteSearch360Props>
+interface SiteSearch360Props {
+	ss360Config?: object;
+	siteId: string;
+	alias?: string;
+	showButton?: boolean;
+	applyStyling?: boolean
 }
+
+declare class SiteSearch360 extends React.Component {
+	props: SiteSearch360Props;
+    ensureConfig(): void;
+    componentDidMount(): void;
+    render(): any;
+}
+
+export default SiteSearch360;
